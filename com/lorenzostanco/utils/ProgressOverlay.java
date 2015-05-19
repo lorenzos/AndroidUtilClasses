0ba161dca5e674756c0365004eaf98c6bf765b36
Lorenzo Stanco <lorenzo.stanco@gmail.com>
2015-05-19 18:20:35 +0200

    ProgressOverlay custom color for API 21+

--- com/lorenzostanco/utils/ProgressOverlay.java
+++ com/lorenzostanco/utils/ProgressOverlay.java
@@ -1,8 +1,12 @@
 package com.lorenzostanco.utils;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Build;
 import android.widget.ProgressBar;
 
 /**
@@ -10,6 +14,9 @@ import android.widget.ProgressBar;
  * {@code overlay = ProgressOverlay.show(this); ... overlay.dismiss()}
  */
 public class ProgressOverlay {
+	
+	/** The color of the circular spinner on API 21+ (null to use the default one) */
+	public static Integer MATERIAL_COLOR = Color.WHITE;
 	
 	/** Creates and shows a progress dialog with no borders, frame nor text over the default semi-transparent dialogs' background.
 	 * @return ProgressDialog The new dialog instance */
@@ -25,9 +32,11 @@ public class ProgressOverlay {
 
 	/** Creates and shows a progress dialog with no borders, frame nor text over the default semi-transparent dialogs' background.
 	 * @return ProgressDialog The new dialog instance */
-	public static ProgressDialog show(final Context context, final boolean cancelable, final DialogInterface.OnCancelListener cancelListener) {
+	@SuppressLint("NewApi") public static ProgressDialog show(final Context context, final boolean cancelable, final DialogInterface.OnCancelListener cancelListener) {
 		final ProgressDialog dialog = ProgressDialog.show(context, null, null, true, cancelable, cancelListener);
-		dialog.setContentView(new ProgressBar(context));
+		ProgressBar view = new ProgressBar(context);
+		if (MATERIAL_COLOR != null && Build.VERSION.SDK_INT >= 21) view.setIndeterminateTintList(ColorStateList.valueOf(MATERIAL_COLOR));
+		dialog.setContentView(view);
 		dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
 		return dialog;
 	}
